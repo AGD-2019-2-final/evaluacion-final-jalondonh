d352fdb83124aa36616f4517532aaaeda02f29c0
jalondonh@unal.edu.co <jalondonh@unal.edu.co>
2020-01-29 14:35:59 -0500

    jalhq03

--- 01-hadoop-50/q03-10/mapper.py
+++ 01-hadoop-50/q03-10/mapper.py
@@ -1,4 +1,13 @@
 import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import csv
+if __name__ == '__main__':
+
+    file = sys.stdin.readlines()
+
+    for line in csv.reader(file):
+        key = line[0]
+        value = line[1]
+
+        sys.stdout.write("{},{}\n".format(value,key))
--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -1,4 +1,10 @@
 import sys
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import re
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+
+        linere = re.sub("\n","",line)
+        lista = linere.split(",")
+
+        sys.stdout.write("{},{}\n".format(lista[1],lista[0]))
